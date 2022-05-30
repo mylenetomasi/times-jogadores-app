05fbd893c1fe3400097e9bbb4503be7a902f62cd
ana paula <aninha10.desouza@gmail.com>
2022-05-30 15:13:03 -0300

    ok

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>Home | Jogadores | Times</header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  height: 100vh;
+
+  font-weight: normal;
+}
+header,
+footer {
+  background-color: rgb(59, 59, 127);
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+main {
+  height: 70%;
+}
+</style>
